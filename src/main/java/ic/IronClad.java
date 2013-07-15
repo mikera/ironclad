dfbeb0ad8615f9c7bf76a607244b0105e274fcbd
mikera <mike.r.anderson.13@gmail.com>
2013-07-15 09:55:24 +0100

    format main class

--- src/main/java/ic/IronClad.java
+++ src/main/java/ic/IronClad.java
@@ -11,43 +11,43 @@ import java.awt.Label;
 @SuppressWarnings("serial")
 public class IronClad extends JApplet {
 
-	public static JApplet applet=null;
+	public static JApplet applet = null;
 
-	public static boolean JAVA_LAUNCHED=false;
-	public static boolean START_SCREEN=true;
-	public static boolean DEBUG_MODE=true;
-		
+	public static boolean JAVA_LAUNCHED = false;
+	public static boolean START_SCREEN = true;
+	public static boolean DEBUG_MODE = true;
+
 	public IronClad() {
-		applet=this;
-		
+		applet = this;
+
 	}
-	
+
 	public void init() {
-		setBackground ( Color.black ); 
+		setBackground(Color.black);
 		try {
 			main(null);
 		} catch (Exception e) {
 			getContentPane().add(new Label(e.toString()));
 		}
 	}
-	
+
 	public void update(Graphics g) {
-        paint(g);
-    }
-	
+		paint(g);
+	}
+
 	public void paint(Graphics g) {
-        super.paint(g);
-    }
-	
+		super.paint(g);
+	}
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) throws Exception {
-		JAVA_LAUNCHED=true;
-		START_SCREEN=true;
-		DEBUG_MODE=false;
+		JAVA_LAUNCHED = true;
+		START_SCREEN = true;
+		DEBUG_MODE = false;
 		System.setSecurityManager(null);
-		
+
 		Clojure.require("ic.main");
 		Clojure.eval("(ic.main/main)");
 	}
